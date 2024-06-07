0f390f076762470f9d967b0bfbcec357fa39edf9
Ayesh Irunda <cohndse232f-100@student.nibm.lk>
2024-06-07 16:53:31 +0530

    initilize the swipper

--- public/index.js
+++ public/index.js
@@ -11,3 +11,21 @@ function menu(e){
         navbarr.classList.remove('opacity-100');
     }
 }
+//initilize the swipper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
