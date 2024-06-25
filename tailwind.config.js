bc7d38c85ea168b5370fddec63f1a1fb62f5522e
Ayesh Irunda <cohndse232f-100@student.nibm.lk>
2024-06-26 00:55:36 +0530

    make responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -329,6 +329,7 @@ module.exports = {
       xs: ['0.75rem', { lineHeight: '1rem' }],
       sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
+      base2: ['1.10rem', { lineHeight: '1.60rem' }],
       lg: ['1.125rem', { lineHeight: '1.75rem' }],
       xl: ['1.25rem', { lineHeight: '1.75rem' }],
       '2xl': ['1.5rem', { lineHeight: '2rem' }],
