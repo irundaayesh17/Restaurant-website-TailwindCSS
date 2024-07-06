5ec8f5ef3d804e183c2d2b3e34ac38f2e08910f0
Ayesh <ayeshirunda1234@gmail.com>
2024-07-06 07:21:02 +0530

    Revert "aadded firebase"
    This reverts commit 953579a4c1909642d15d9dedd3dd55a4cb07cc38.

--- public/register.js
+++ public/register.js
@@ -1,54 +1,4 @@
-let navbarr = document.querySelector('ul');
 
-function menu(e){
-    if(e.name === 'menu'){
-        e.name = 'close';
-        navbarr.classList.toggle('opacity-100');
-        navbarr.style.zIndex = '1000';
-        navbarr.style.pointerEvents = 'all'; // Enable pointer events when navbar is visible
-    }
-    else{
-        e.name = 'menu';
-        navbarr.classList.remove('opacity-100');
-        navbarr.style.zIndex = '-1';
-        navbarr.style.pointerEvents = 'none'; // Disable pointer events when navbar is invisible
-    }
-}
-
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.style.backgroundImage = "url('https://i.ibb.co/VWsBT2J/headerbg.png')";
-    } else {
-        header.style.backgroundImage = "";
-    }
-});
-
-//navigations
-//navigaaton header
-document.getElementById('menu').addEventListener('click', function() {
-    window.location.href = 'menu.html';
-  });
-  document.getElementById('about').addEventListener('click', function() {
-    window.location.href = 'about.html';
-  });
-  document.getElementById('register').addEventListener('click', function() {
-    window.location.href = 'register.html';
-  });
-  document.getElementById('login').addEventListener('click', function() {
-    window.location.href = 'login.html';
-  });
-  document.getElementById('home').addEventListener('click', function() {
-    window.location.href = 'index.html';
-  });
-  document.getElementById('contact').addEventListener('click', function() {
-    window.location.href = 'contact.html';
-  });
-  document.getElementById('order').addEventListener('click', function() {
-    window.location.href = 'order.html';
-  });
-
-  //Registration form
 
 const showpass = document.getElementById('showpass');
 
@@ -194,4 +144,52 @@ function passwordvalidation() {
     alert('Registration Successful');
 }     
 
-//USER REGISTRATION
+let navbarr = document.querySelector('ul');
+
+function menu(e){
+    if(e.name === 'menu'){
+        e.name = 'close';
+        navbarr.classList.toggle('opacity-100');
+        navbarr.style.zIndex = '1000';
+        navbarr.style.pointerEvents = 'all'; // Enable pointer events when navbar is visible
+    }
+    else{
+        e.name = 'menu';
+        navbarr.classList.remove('opacity-100');
+        navbarr.style.zIndex = '-1';
+        navbarr.style.pointerEvents = 'none'; // Disable pointer events when navbar is invisible
+    }
+}
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.backgroundImage = "url('https://i.ibb.co/VWsBT2J/headerbg.png')";
+    } else {
+        header.style.backgroundImage = "";
+    }
+});
+
+//navigations
+//navigaaton header
+document.getElementById('menu').addEventListener('click', function() {
+    window.location.href = 'menu.html';
+  });
+  document.getElementById('about').addEventListener('click', function() {
+    window.location.href = 'about.html';
+  });
+  document.getElementById('register').addEventListener('click', function() {
+    window.location.href = 'register.html';
+  });
+  document.getElementById('login').addEventListener('click', function() {
+    window.location.href = 'login.html';
+  });
+  document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
+  document.getElementById('contact').addEventListener('click', function() {
+    window.location.href = 'contact.html';
+  });
+  document.getElementById('order').addEventListener('click', function() {
+    window.location.href = 'order.html';
+  });
